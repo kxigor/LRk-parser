4f4ae6a4dfd4100ac0acf08413a44b53fbfcccd6
Garik11 <garik.irk@mail.ru>
2025-11-30 13:16:43 +0300

    Набросок грамматики

--- include/lrk_parser.hpp
+++ include/lrk_parser.hpp
@@ -1,9 +1,15 @@
 #pragma once
 
+#include <algorithm>
 #include <cstddef>
+#include <format>
+#include <functional>
+#include <stdexcept>
 #include <string>
+#include <string_view>
 #include <unordered_map>
 #include <unordered_set>
+#include <utility>
 #include <vector>
 
 namespace lrk_parser {
@@ -11,6 +17,7 @@ class LrkParser {
   /*====================== Usings/Helpers ======================*/
   using CharT = char;
   using StringT = std::basic_string<CharT>;
+  using StringViewT = std::basic_string_view<CharT>;
 
   template <typename T>
   using VectorT = std::vector<T>;
@@ -24,7 +31,7 @@ class LrkParser {
   struct Situation {
     std::size_t rule_idx{};
     std::size_t dot_pose{};
-    std::size_t lookahead_id{};
+    std::size_t lookahead_idx{};
   };
 
   struct SituationHash {
@@ -34,32 +41,111 @@ class LrkParser {
 
   using Situations = UsetT<Situation, SituationHash>;
 
-  using SymID = std::size_t;
-  using SymIDs = VectorT<SymID>;
-
   struct Rule {
-    SymID lhs{};
-    SymIDs rhs;
+    CharT lhs{};
+    StringT rhs;
   };
-
-  using Rules = VectorT<Rule>;
 
   struct Grammar {
     /*======== Constants =========*/
-    static constexpr SymID kStartID = 0;
+    static constexpr const CharT kStartChar = CharT{'@'};
+    static constexpr const StringViewT kArrow = "->";
+    static constexpr const std::size_t kArrowPos = 1;
+    static constexpr const std::size_t kMinSize = 1 + kArrow.size();
 
     /*========= Factory ==========*/
-    static Grammar init_with_str(StringT terminals, StringT nonterminals,
-                                 VectorT<StringT> rules_str, CharT start) {}
+    static Grammar init_with_strs(StringT terminals, StringT nonterminals,
+                                  VectorT<StringT> rules_str, CharT start) {
+      prepare_rules_str(rules_str);
+
+      Grammar grammar = {
+          .terminals{terminals.begin(), terminals.end()},
+          .nonterminals{nonterminals.begin(), nonterminals.end()}};
+
+      grammar.throw_if_wrong_terminal_nontermianls();
+
+      grammar.add_rule(kStartChar, {start});
+
+      for (const auto& rule_str : rules_str) {
+        grammar.add_rule(rule_from_str(rule_str));
+      }
+      return grammar;
+    }
+
+    /*========== Impls ===========*/
+    static Rule rule_from_str(const StringT& rule_str) {
+      return Rule{.lhs = rule_str[0],
+                  .rhs = {rule_str.begin() + kMinSize, rule_str.end()}};
+    }
+
+    void add_rule(CharT lhs, StringT rhs) {
+      add_rule(Rule{.lhs = std::move(lhs), .rhs = std::move(rhs)});
+    }
+
+    void add_rule(Rule rule) {
+      const std::size_t kNewRuleIdx = rules.size();
+      lhs_to_rule_idxs.emplace(rule.lhs, kNewRuleIdx);
+      rules.emplace_back(std::move(rule));
+    }
+
+    void throw_if_wrong_terminal_nontermianls() const {
+      if (terminals.contains(kStartChar) or nonterminals.contains(kStartChar)) {
+        throw std::logic_error(
+            "the @ symbol is reserved by the grammar, it cannot be used");
+      }
+
+      for (const auto& terminal : terminals) {
+        if (nonterminals.contains(terminal)) {
+          throw std::logic_error(
+              "the set of terminal and non-terminal symbols cannot overlap");
+        }
+      }
+    }
+
+    void throw_if_wrong_rule_str(const StringT& rule_str) const {
+      /*TODO: improve code*/
+
+      if (rule_str.size() < kMinSize) {
+        throw std::logic_error("str rule requires a size of at least");
+      }
+      if (auto find_res = rule_str.find(kArrow); find_res != kArrowPos) {
+        throw std::logic_error("arrow missing or in the wrong place");
+      }
+      const auto& lhs_sym = rule_str[0];
+
+      if (terminals.contains(lhs_sym)) {
+        throw std::logic_error(
+            "there can't be a terminal on the left of the rule");
+      }
+
+      for (std::size_t i = 0; i < kArrowPos; ++i) {
+        if (not terminals.contains(rule_str[i]) and
+            not nonterminals.contains(rule_str[i])) {
+          throw std::logic_error("an unknown symbol has been encountered");
+        }
+      }
+
+      for (std::size_t i = kArrowPos + kArrow.size(); i < rule_str.size();
+           ++i) {
+        if (not terminals.contains(rule_str[i]) and
+            not nonterminals.contains(rule_str[i])) {
+          throw std::logic_error("an unknown symbol has been encountered");
+        }
+      }
+    }
+
+    static void prepare_rules_str(VectorT<StringT>& rules_str) {
+      for (auto& rule_str : rules_str) {
+        std::ranges::remove_if(rule_str,
+                               [](auto& sym) { return std::isspace(sym); });
+      }
+    }
 
     /*======= Data fields ========*/
-    UmapT<CharT, SymID> sym_to_id_;
-    UmapT<SymID, CharT> id_to_sym_;
-    UsetT<SymID> terminals_;
-    UsetT<SymID> nonterminals_;
-    Rules rules_;
-    UmapT<SymID, VectorT<std::size_t>> lhs_id_to_rule_idxs;
-    VectorT<SymIDs> lookahead_pool_;
+    UsetT<CharT> terminals;
+    UsetT<CharT> nonterminals;
+    VectorT<Rule> rules;
+    UmapT<CharT, VectorT<std::size_t>> lhs_to_rule_idxs;
   };
 };
 }  // namespace lrk_parser
